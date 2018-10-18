181e270fb94134f5fc39486abdfc20942294255a
sunwenwu <sunwenwu@winchannel.net>
2018-10-18 13:50:20 +0800

    新增日志功能

--- src/main/java/com/example/firstspringboot/controller/HelloController.java
+++ src/main/java/com/example/firstspringboot/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.firstspringboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
     //访问/hello或者/hi任何一个地址，都会返回一样ss的结果
     @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
     public String say(){
+        logger.info("log test...");
         return "hi you!!!";
     }
 }
