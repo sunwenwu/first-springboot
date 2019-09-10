dd51121cbc410e257b0c33ebb476dc193243a05f
sunwenwu <sunwenwu@ipaynow.cn>
2019-09-10 10:23:16 +0800

    payTest

--- src/main/java/com/example/firstspringboot/common/alipay/AliPayUtils.java
+++ src/main/java/com/example/firstspringboot/common/alipay/AliPayUtils.java
@@ -9,7 +9,9 @@ import com.alipay.api.domain.AlipayTradeAppPayModel;
 import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.internal.util.StringUtils;
 import com.alipay.api.request.AlipayTradeAppPayRequest;
+import com.alipay.api.request.AlipayTradeQueryRequest;
 import com.alipay.api.response.AlipayTradeAppPayResponse;
+import com.alipay.api.response.AlipayTradeQueryResponse;
 import org.apache.commons.lang3.builder.ToStringExclude;
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +24,8 @@ public class AliPayUtils {
 
     public static void main(String[] args) throws Exception{
 
-      run5();
+//      run5();
+        runQuery();
     }
 
     /**
@@ -89,6 +92,50 @@ public class AliPayUtils {
         }
     }
 
+
+    /**
+     * 调用失败
+     * {"alipay_trade_query_response":{"code":"40004","msg":"Business Failed","sub_code":"ACQ.TRADE_NOT_EXIST","sub_msg":"交易不存在","buyer_pay_amount":"0.00","invoice_amount":"0.00","out_trade_no":"201801364481792961421312","point_amount":"0.00","receipt_amount":"0.00"},"sign":"II+b38B8MliBEpKSgJzLSMiS2jPNxMpA4F9uS4Y4eLunLoh+XvvS6PeCZi3uqDbvi+R5QgCGkowvi/Na0V+GDz1gS3Y0TPBPiWpDqhdW96Zm0jCrWsPyZyqDBW7vF9QHCypmiBe6ZdnwMQkCRVbLOksA1RElYSodwv3s2FTvq1wVOYbIgrRaS8g/ORK8rECgopJIvmifbD9ta8N6joJ4HaJm9cr37ZWKACcavZVm/VoY3ohSkO2Yss7dbl/0mOVnDA3/TYyuHowsUbcryZxolqAURI4FRQgO81jHIF8v/tjY4xcr8jtoVJl7uSw0xHXEHeJ4JsuG53C/VttjqgOA7A=="}
+     * @throws Exception
+     */
+
+    public static void runQuery() throws Exception{
+        String appid = "2015120700927798";
+        String APP_PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCUbscpo605LV8J7EQ4b68aebURF0wJB8c5qwyEGYGFZi8NbaXXInbS4CU+V5d0OqMGYUOJkZWatuxpuYg2Y7bo6PLbWJfoBnYs5egt2Xisilkt9kAPWd2kIHeEMbYk30hUcBxI3syvdKiB24kRw45p3CRsy10AUMJdxjiKxErSrJw3uURcjN7Utwt7EsGb/shk5TI0X3XNI8yLvPaO/tIN3IStUDXHc/4VzkNN3Pt0Di4q62mIMDmQu4U5dO/9qJR3rtipOh7iR459SGMm6fvbYnirog831A5yTqRPe2EXaRiI+oKQ+48ER12hQk7FlGXBHGWbMKsXDDxU6jN2jnoFAgMBAAECggEAa1Ywb8wM1+wXbfWM38LjvSGF7nYKZf1Z9DndMym+n9jk7bD83DXv905foFV3EHXWriZn0HOW2+qry0h/V9WDz6mk9Tl7oC4MByu6Wm2I9jyVgMcyS+eQf796rf3E2aUdiPIJgiibm8WglUdMN6xSr/qh3IqzFidQZr+Q48hO00kk1AkFORHjIwWDCW1ftzTVCAyGMigt7mukSt0SBjJ23XBbcP9A3M1HUvImO8/jwYXp+8FSekDhHsMru6DSXYlmHJXRDETwG61BkBKauCi6q9V+QON4zWAncHgNJ6ULJ8O63CxylhmbVgnlwMLeVUHCVgprwAiWjLtAslw6nX5YAQKBgQDN7qxSYOpPqttFOj+uZfjRK4uO7yaM6wMnl5sHLfw9kyGpwdaUdjnAxokGwFpe7lUu3CfpAsl4RPUzB0IjwO4GwwqDq0cfd8tV1X1WH1L0tjV1F6wfMX8jXobTfQyD1SKX7AcN55K1BJT6pzAbqcd0X3d/Lu05zYqSzdRM7JCK1QKBgQC4hU10/yPgSwAKn6JHvAx32IrTKJ+V9o8r1S8WxcLP2yUG/adXLuzzU3RKTkxWP6nliweHzhG8HLwQ3Omaoujb+ibwCF/qq35PfKSeV/MO/Eth1ifBFvbFponF7wdlDf4PvRAR8nY9er39oCOpuwAnW1o3b5xXaplFXtOgZLFqcQKBgQCq4Kpy7OJg2wh5qDzhTNIrVvPF1iHp+rSybLWCPqE4wtbDEwooRPXlRZi8Vm5mqWNbr+kaC2r79lR0VEiIfHXGfJ9D5KLutKuv/Ke1B+cBEhTHpa34cEzsH2rHINN6rSe4Ymz0F7z/od6xInvj6HyNzw3MJjdIqIqdCFhqwD2sRQKBgQCZs7iMr4bz66WSM90awgLkxcUGFb2xTlt+WKtL3dnkCJi0VRBSqC0Kl0a0Krqd0ZQ1N/VC452UQ59IsKwKeo9bxekvYtp7APVLQvlhSMOXhQNJ8PjWd8mBZLM45F4ViG1MMOG1AXIKzjZ6kS2D0dhT8F2a9eaWTp5z0YSZA6dhkQKBgAsI02IC3BygZbTiFdbgWvqiham8qJ5jH5QNI1RHCVYsHQBYrgmafhmo3iQkBqItWp3df/vnZFXcc5fULXNBj8UAZ6KNNHzNPn2tbWtlTaS2GCuCN7vPxVjGnm7pT8S52c3nZlBj79GJQrhM+i5+vlPOmxQH/dcUdEUvGmza28ht";
+
+        String ALIPAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsYfjWGvbFQ89IplTye/+rVa8oZBvH6x+hQQYWF2ncoduvZSL0FezLN+hk9h9TnHAq5sOHomnr8Hz77peeqDUJPjJJLz+Nad+bRc5QcAPEREBMso8FvZOUYmb1ZoPE3heKTgzFW16cx0XPVTZc/fkOVc9zU4rSeDjbdpPc5bVIZ7Uk5uyjATi3UdmlhUEjZQb0LDGKauYbcJ8tPYgnWh+/1QWUV/0AiWvZSAo9chPLda2DctaEcFou/W2kA/HK60TNL1HzgMZSfShoXfomxJIaSOTwT0BeL7Gkw4zjD/AqfyiYCiwzOpNTLT6oX+FqmXwvJxar15kqggLL4Ke8tNI/QIDAQAB";
+
+
+        AlipayClient alipayClient = new DefaultAlipayClient("https://openapi.alipay.com/gateway.do",appid,APP_PRIVATE_KEY,"json","GBK",ALIPAY_PUBLIC_KEY,"RSA2");
+        AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+        request.setBizContent("{" +
+                "\"out_trade_no\":\"201801364096081330446336\"}");
+/*
+        request.setBizContent("{" +
+                "\"out_trade_no\":\"201801364481792961421312\"}");
+*/
+        AlipayTradeQueryResponse response = alipayClient.execute(request);
+        if(response.isSuccess()){
+            System.out.println("调用成功");
+        } else {
+            System.out.println("调用失败");
+        }
+
+        String body = response.getBody();
+
+        System.out.println(body);
+        JSONObject jsonObject = JSONObject.parseObject(response.getBody());
+        String rspObj = jsonObject.getString("alipay_trade_query_response");
+
+        System.out.println(response.getTradeStatus());
+        Map<String,String> resultMap = JSONObject.parseObject(rspObj, Map.class);
+        resultMap.put("sign",jsonObject.getString("sign"));
+
+        boolean flag = AlipaySignature.rsaCheckV1(resultMap, ALIPAY_PUBLIC_KEY, "UTF-8","RSA2");
+
+        System.out.println("验签结果："+flag);
+
+    }
 
     public static void run3()throws Exception{
         String data = "{\"gmt_create\":\"2019-08-26 15:58:33\",\"charset\":\"UTF-8\",\"seller_email\":\"mds.online@cn.mcd.com\",\"subject\":\"测试订单\",\"sign\":\"qYSmfxGuR4by3yBpd9UF12hpEz58ITmdWLXiLH5EEeFma8hFJhTks2ReAenteYnL7Nw6Ee9K2d+mqd+J5jAiDp/qwW9fhPtjHvqznCfK0458o7jgwWRgJx4+h1S9MCMb7wON8P7hg1e5WeHEwDuMxWnzs6yZjpoLAzOddB5Vm5IGYkv9JlXQCG2DI8Ely1AYglNlzLMQQt6NS6dUm+mz/OAYmr5ZTD1UcIQnjL8BUQGNoAhTpqWgE4hXsNNoqoxaFrxcT9LOyPBZsEsx+ola2+cRmq/YMK2E2rQTQHKrCX+m1ivwxa5vjT4SMkdVMJ7kXJGGIWSyOk/U1jNPbCn5ow==\",\"buyer_id\":\"2088502971826745\",\"invoice_amount\":\"0.01\",\"notify_id\":\"2019082600222155834026740558735815\",\"fund_bill_list\":\"[{\\\"amount\\\":\\\"0.01\\\",\\\"fundChannel\\\":\\\"ALIPAYACCOUNT\\\"}]\",\"notify_type\":\"trade_status_sync\",\"trade_status\":\"TRADE_SUCCESS\",\"receipt_amount\":\"0.01\",\"app_id\":\"2015120700927798\",\"buyer_pay_amount\":\"0.01\",\"sign_type\":\"RSA2\",\"seller_id\":\"2088511432039190\",\"gmt_payment\":\"2019-08-26 15:58:34\",\"notify_time\":\"2019-08-26 15:58:34\",\"version\":\"1.0\",\"out_trade_no\":\"201801363393765660958720\",\"total_amount\":\"0.01\",\"trade_no\":\"2019082622001426740554386869\",\"auth_app_id\":\"2015120700927798\",\"buyer_logon_id\":\"158****6877\",\"point_amount\":\"0.00\"}";
@@ -201,4 +248,10 @@ public class AliPayUtils {
 
         System.out.println(jsonObject.getString("sign"));
     }
+
+
+
+
+
+
 }
